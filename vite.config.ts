d12538efcd259918fdb64d15e8ebfd39b4d217bd
KAE REN <hkr0528@gmail.com>
2025-09-30 06:41:19 +0800

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/", // 👈 needs to be outside of resolve, and comma added above
 });
